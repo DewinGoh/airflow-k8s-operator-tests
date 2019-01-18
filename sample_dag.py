4106b36e9a2dddb773e53da55fb5f8e54c8c4cfd
Dewin Zhong Zhe Goh <dewin.goh@grabtaxi.com>
2019-01-18 08:45:55 +0800

    change sample dag

--- sample_dag.py
+++ sample_dag.py
@@ -7,31 +7,24 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 default_args = {
     'owner': 'Dewin Goh',
     'depends_on_past': False,
-    'start_date': datetime(2019, 1, 17, 1),
+    'start_date': datetime(2019, 1, 18, 1),
     'retries': 1,
     'concurrency': 1,
     'max_active_runs': 1
 }
 
-arguments = """
-<<EOF
-import os
-print(os.listdir())
-EOF
-"""
 
 dag = DAG('cloudfront-sg-update.update_sg_dag', default_args=default_args, schedule_interval='*/1 * * * *')
 job_name = 'cloudfront-sg-update.update-sg'
 
 the_task = KubernetesPodOperator(namespace="airflow",
                           image="Python:3.6",
-                          cmds=["python","-"],
-                          arguments=[arguments],
+                          cmds=["python"],
+                          arguments=["./scripts/sample_code.py"],
                           name="dag-test",
                           task_id="dag-test-task-id",
                           get_logs=True,
                           annotations="iam.amazonaws.com/role: api_access_role",
                           dag=dag
                           )
-
 
---
+++ scripts/sample_code.py
@@ -0,0 +1,7 @@
+import os, sys
+
+def print_cwd():
+	print(os.listdir())
+
+if __name__ == '__main__':
+	print_cwd()
