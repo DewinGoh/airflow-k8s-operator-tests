a2d986bd9e09a0aedfbe84b84d91bfb74556f621
Dewin Zhong Zhe Goh <dewin.goh@grabtaxi.com>
2019-02-13 14:27:06 +0800

    add dags folder

---
+++ dags/sample_dag.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from kubernetes import config
+
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+import logging
+
+
+default_args = {
+    'owner': 'Dewin Goh',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 1, 18, 1),
+    'retries': 1,
+    'concurrency': 1,
+    'max_active_runs': 1
+}
+
+
+dag = DAG('cloudfront-sg-update.update_sg_dag', default_args=default_args, schedule_interval='* * * * *')
+job_name = 'cloudfront-sg-update.update-sg'
+
+
+the_task = KubernetesPodOperator(namespace="airflow-test",
+                                image="python:3.6.3",
+                                cmds=["python","-c"],
+                                arguments=["print('Hello World!')"],
+                                in_cluster=True,
+                                name="dag-test",
+                                task_id="dag-test-task-id",
+                                get_logs=True,
+                                annotations={"iam.amazonaws.com/role": "api_access_role"},
+                                is_delete_operator_pod=True,
+                                dag=dag
+                                )
